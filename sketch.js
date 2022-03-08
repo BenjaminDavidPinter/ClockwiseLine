fdec877db14505b968571278b57f88923e946459
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2022-03-07 22:13:07 -0500

    Refining

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let X = 0;
 let Y = 0;
 
-let radius = 1000000;
+let radius = 100000000;
 let angle = 10;
 
 let mainPivotX = 300;
@@ -11,21 +11,46 @@ let lastPointX = 0;
 let lastPointY = 0;
 
 const locations = [];
+const intersections = [];
 
 function setup() {
+	mainPivotX = windowWidth/2;
+	mainPivotY = windowHeight/2;
 	createCanvas(windowWidth, windowHeight);
-	frameRate(144);
 	strokeWeight(4);
+	for (let index = 0; index < 4; index++) {
+		locations.push({ x: Math.floor(Math.random() * windowWidth), y: Math.floor(Math.random() * windowHeight), tolerance: 0, active: false })		
+	}
+	mainPivotX = locations[2].x;
+	mainPivotY = locations[2].y;
+	locations[2].active = true;
 }
 
 function draw() {
 	clear();
 	drawLineOnPivot(mainPivotX, mainPivotY, angle);
-	angle = angle + .002;
+	angle = angle + .001;
 
 	locations.forEach(element => {
+		if(element.active){
+			stroke(color('magenta'));
+		}
+		else {
+			stroke(color('black'));
+		}
 		ellipse(element.x, element.y, 10);
+		stroke(color('white'));
+		text('Tolerance: ' + element.tolerance, element.x, element.y + 25);
+		stroke(color('black'));
 	});
+
+	strokeWeight(2);
+	intersections.forEach(coords => {
+		stroke(color('blue'));
+		line(coords.X1, coords.Y1, coords.X2, coords.Y2);
+	});
+	stroke(color('black'));
+	strokeWeight(4);
 
 
 }
@@ -43,13 +68,25 @@ function drawLineOnPivot(pivotX, pivotY, angle) {
 	var locationSet = false;
 	locations.forEach(loc => {
 		if (!locationSet) {
+			
+			//don't hop to previous point
 			if (mainPivotX == loc.x
 				&& mainPivotY == loc.y) {
 				return;
 			}
 
-			if (Math.ceil(slope1 * loc.x + yintercept1) >= loc.y - 5
-				&& Math.floor(slope1 * loc.x + yintercept1) <= loc.y + 5) {
+			//collision tolerance should be a function of the distance between current x,y and target x,y
+			//The farther away it is, the larger the tolerance needs to be.
+			//Max tolerance is 10 pixels
+			loc.tolerance = (((Math.abs(pivotX-loc.x) + Math.abs(pivotY-loc.y)) / (windowWidth + windowHeight))*15);
+			if(loc.tolerance == 6){
+				loc.tolerance = 7;
+			}
+
+
+			//intercept
+			if (Math.ceil(slope1 * loc.x + yintercept1) >= loc.y - loc.tolerance
+				&& Math.floor(slope1 * loc.x + yintercept1) <= loc.y + loc.tolerance) {
 				if(lastPointX == loc.x && lastPointY == loc.y && locations.length > 2){
 					return;
 				}
@@ -58,13 +95,18 @@ function drawLineOnPivot(pivotX, pivotY, angle) {
 				mainPivotX = loc.x;
 				mainPivotY = loc.y;
 				locationSet = true;
-				angle = angle + 5;
+				loc.tolerance = 0;
+				loc.active = true;
+				intersections.push({X1:pivotX, Y1:pivotY, X2:loc.x, Y2:loc.y});
+			}
+			else {
+				loc.active = false;
 			}
 		}
 	})
 }
 
 function mouseClicked() {
-	locations.push({ x: mouseX, y: mouseY });
+	locations.push({ x: mouseX, y: mouseY, tolerance: 0, active: false });
 	console.log(mouseX, mouseY);
 }
